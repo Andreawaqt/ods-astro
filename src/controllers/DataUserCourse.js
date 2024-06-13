62a9a17e1d281af0bf90d7ae3eb96fdb93a7b46e
adrianvicoh <1hiaw.vico.adrian@gmail.com>
2024-06-13 16:33:43 +0200

    Completado DataUserCourse

--- src/controllers/DataUserCourse.js
+++ src/controllers/DataUserCourse.js
@@ -1,7 +1,7 @@
 import BaseController from "./BaseController";
 import CourseController from "./CourseController";
 import UserController from "./UserController";
-import data from "../data/data.json"
+import fullData from "../data/data.json"
 
 class DataUserCourse {
 
@@ -26,16 +26,24 @@ class DataUserCourse {
         let course = new CourseController(url, this.course);
         // Obtener datos completos del curso
         let allCourseData = await course.getCourse();
-
         // Obtener los datos concretos del usuario y usar shift para quitar el id
-        let userCourseData = allCourseData.filter(data => data.userID === this.userID).shift()
+        let userCourseData = allCourseData.filter(data => data.userID === this.userID)
+        console.log(userCourseData)
+        // Crear un array con los títulos de las unidades formativas desde fullData
+        let fullContent = fullData[this.course];
         // Crear un array de objetos con el título y un boleano que indique si está completado
-        let completedUnities = [];
-        // Instanciar userCourseData para añadir los datos corerspondientes
+        let completedUnities = [
+            {completed: userCourseData[0].uf1, description: fullContent[0].unidad },
+            {completed: userCourseData[0].uf2, description: fullContent[1].unidad },
+            {completed: userCourseData[0].uf3, description: fullContent[2].unidad },
+            {completed: userCourseData[0].uf4, description: fullContent[3].unidad }
+        ];
+        /*Instanciar userCourseData para añadir los datos corerspondientes
         for(let i = 1; i < 4; i++) {
-            completedUnities.push({completed:userCourseData.uf1[1]})
-        }
+            completedUnities.push({completed:userCourseData.uf1})
+        }*/
         console.log(completedUnities);
+        return completedUnities;
     }
 
 }
